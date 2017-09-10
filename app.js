2a6632cf0943a92e329ca399895d1586ab9e0a61
geralle <gerallestes@gmail.com>
2017-09-10 17:19:50 -0600

    fixed player-container

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 startButton = document.getElementById('play-button')
+playerContainer = document.getElementById('player-container')
+console.log(playerContainer.offsetWidth)
 
 // start button
 startButton.addEventListener('click',function(event){
   event.preventDefault()
   dropPoint();
-
 })
+var position = 0;
+
+window.addEventListener("keydown",function(event){
+  if(event.keyCode===37 && position > 0){
+    position = position-50
+    console.log(position)
+    var pointGrabber = document.getElementById("point-grabber");
+    pointGrabber.setAttribute('style','margin-left:'+ position + "px")
+  }else if(event.keyCode===39){
+    width = playerContainer.offsetWidth - 50
+    // console.log(width)
+    if(position<width){
+      position = position+50
+      console.log(position)
+      var pointGrabber = document.getElementById("point-grabber");
+      pointGrabber.setAttribute('style','margin-left:'+ position + "px")
+    }
+  }
+});
 
 function dropPoint(){
   var randomColumn = Math.floor(Math.random() * 12);
