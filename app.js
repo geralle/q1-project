13e9d0b697b29a36e9c932715fd79cda077b891d
geralle <gerallestes@gmail.com>
2017-09-11 12:18:38 -0600

    before gameloop

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var position = 0;
 var moveOver = 30;
+var userScore = 0;
+var pointId = 0
+var ptsDisplayed = []
 
-// start button
 startButton = document.getElementById('play-button')
 startButton.addEventListener('click',function(event){
   event.preventDefault()
@@ -25,44 +27,67 @@ window.addEventListener("keydown",function(event){
 });
 
 function dropPoint(){
+  pointId++;
   var randomColumn = Math.floor(Math.random() * 12)+1;
   var column = document.getElementById("column-"+randomColumn);
   var pointIcon = document.createElement('div');
-  pointIcon.setAttribute('id','point-icon')
+  pointIcon.setAttribute('id','point-' + pointId)
+  pointIcon.setAttribute('class','points')
   pointIcon.setAttribute('style',
-      `animation: point-drop 5s;
+      `border:4px solid purple;
+      animation: point-drop 5s;
       position: absolute;
       background-color: blue;
       height: 20px;
       width: 20px;`)
   column.append(pointIcon);
+  var pointIcon = document.getElementsByClassName("points")
+  ptsDisplayed.push(pointIcon)
+  // getPointsDisplay(ptsDisplayed)
   var timeout = setTimeout(function(){
     column.innerText = ""
+    ptsDisplayed.shift()
   },5000);
 }
 
-// function collision($div1, $div2) {
-//       var x1 = $div1.offset().left;
-//       var y1 = $div1.offset().top;
-//       var h1 = $div1.outerHeight(true);
-//       var w1 = $div1.outerWidth(true);
-//       var b1 = y1 + h1;
-//       var r1 = x1 + w1;
-//       var x2 = $div2.offset().left;
-//       var y2 = $div2.offset().top;
-//       var h2 = $div2.outerHeight(true);
-//       var w2 = $div2.outerWidth(true);
-//       var b2 = y2 + h2;
-//       var r2 = x2 + w2;
-//
-//       if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
-//       	return false;
-//       }else{
-//       	return true;
-//       }
-// }
-//
-//
-// window.setInterval(function() {
-//     $('#result').text(collision($('#div1'), $('#div2')));
-// }, 200);
+var pts = function getPointsDisplay(pts){
+  return pts
+}
+
+function collision($points, $detector) {
+  var x1 = $points.offset().left;
+  var y1 = $points.offset().top;
+  var h1 = $points.outerHeight(true);
+  var w1 = $points.outerWidth(true);
+  var b1 = y1 + h1;
+  var r1 = x1 + w1;
+  var x2 = $detector.offset().left;
+  var y2 = $detector.offset().top;
+  var h2 = $detector.outerHeight(true);
+  var w2 = $detector.outerWidth(true);
+  var b2 = y2 + h2;
+  var r2 = x2 + w2;
+
+  if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+  	return userScore;
+  }else{
+    // console.log($points)
+    userScore++
+  	return userScore;
+  }
+}
+
+function gameOver(){
+  // each point inside of ptsDisplayed
+  // console.log(pts)
+  // for each check if theyre inside player container
+  // if they are / check if theyre touching the paddle
+  // if they are / add a point
+  // else game over
+}
+
+window.setInterval(function() {
+  if(ptsDisplayed.length!=0){
+    $('#result').text(collision($('.points'), $('#point-grabber')));
+  }
+}, 100);
